cd1e7cbc17ab8ef9f3d7035567d484cf77dca71a
Joe Hermaszewski <expipiplus1@gmail.com>
2012-05-11 15:20:33 +0100

    Cleaned things up

--- ancestors.sh
+++ ancestors.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# first, strip underscores
-CLEAN=${1//_/}
-# next, replace spaces with underscores
-CLEAN=${CLEAN// /_}
+# replace spaces with underscores
+CLEAN_NAME=${1// /_}
 # now, clean out anything that's not alphanumeric or an underscore
-CLEAN=${CLEAN//[^a-zA-Z0-9_]/}
+CLEAN_NAME=${CLEAN_NAME//[^a-zA-Z0-9_]/}
 # finally, lowercase with TR
-CLEAN=`echo -n $CLEAN | tr A-Z a-z`
+CLEAN_NAME=`echo -n $CLEAN_NAME | tr A-Z a-z`
 
-gvpr -f reverse.gvpr | dijkstra "$1" -d | gvpr "E[tail.dist > 0]" | gvpr -f reverse.gvpr | dot -Gmclimit=10000.0 -Tpng -o"$CLEAN.png"
+gvpr -f reverse.gvpr        | # Reverse the links reading from stdin
+    dijkstra "$1" -d        | # Compute the distance from the person to each of
+                              # their ancestors
+    gvpr "E[head.dist]"     | # Strip the edges who point to something without 
+                              # a link to the person
+    gvpr -f reverse.gvpr    |
+    dot -Gmclimit=1000.0 -Tpng -o"$CLEAN_NAME.png" # Render the png
