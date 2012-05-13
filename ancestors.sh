02f4775d6076de25751812ed3c13f113445eeb28
Joe Hermaszewski <expipiplus1@gmail.com>
2012-05-13 14:32:45 +0100

    Showing children and spouses

--- ancestors.sh
+++ ancestors.sh
@@ -8,9 +8,15 @@ CLEAN_NAME=${CLEAN_NAME//[^a-zA-Z0-9_]/}
 CLEAN_NAME=`echo -n $CLEAN_NAME | tr A-Z a-z`
 
 gvpr -f reverse.gvpr        | # Reverse the links reading from stdin
-    dijkstra "$1" -d        | # Compute the distance from the person to each of
-                              # their ancestors
-    gvpr "E[head.dist]"     | # Strip the edges who point to something without 
-                              # a link to the person
-    gvpr -f reverse.gvpr    |
-    dot -Gmclimit=1000.0 -Tpng -o"$CLEAN_NAME.png" # Render the png
+dijkstra "$1" -d        | # Compute the distance from the person to each of
+                          # their ancestors
+gvpr -f reverse.gvpr    |
+gvpr -c "BEG_G{}
+         E[tail.dist == 0.0]{head.dist = -2.0;}  // Set the children to -2.0
+         BEG_G{}
+         E[head.dist == -2.0]{tail.dist = -3.0} // Set the parents of the chilren to -3.0
+         BEG_G{ \$tvtype = TV_en; }              
+         E[head.dist == -3.0 && tail.dist == -3.0]{delete(\$G,$);} // remove links between parents
+         N[dist == -1.0]{delete(\$G,$);}         // Remove all the other nodes
+        " |
+dot -Gmclimit=1000.0 -Tpng -o"$CLEAN_NAME.png" # Render the png
